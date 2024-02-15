dcb0e0cf17c7609849b9c01e10643850cbd46509
Maarrcc <marcalvero@insestatut.cat>
2024-02-15 18:50:20 +0100

    canvis

--- operaciones/index.js
+++ operaciones/index.js
@@ -5,8 +5,15 @@
  * @returns {Number} el resultat de la operacio
  */
 function suma(...valors) {
+    let resultat = 0 // Creo una variable per guardar el resultat
+    for (let i = 0; i < valors.length; i++) {
+        // Itero les files de la matriu fins que la variable i sigui menor que la variable valors.length
+        resultat += valors[i] // Afegeixo el valor de l'argument actual a la suma total
+    }
 
+    return resultat
 }
+
 
 
 /**
