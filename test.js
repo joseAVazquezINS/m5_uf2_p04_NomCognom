bccab8014ab2511e1a62348780ea69af3e43e368
Maarrcc <marcalvero@insestatut.cat>
2024-02-15 19:40:24 +0100

    cambios_

--- test.js
+++ test.js
@@ -6,14 +6,14 @@ const op = require("operaciones");
 describe("test operacions", () => {
     test("test suma", () => {
         assert.strictEqual(op.suma(3, 4, 5, 1, 2), 15);
-        assert.strictEqual(op.suma(0, 0), 0);
+        assert.strictEqual(op.suma(), 0);
         assert.strictEqual(op.suma(-1, 1, -1, 1), 0);
         assert.strictEqual(op.suma(-1, 1, 0, -70), -70);
     });
-
+    
     test("test multiplica", () => {
         assert.strictEqual(op.multiplica(3, 4, 5, 1, 2), 120);
-        assert.strictEqual(op.multiplica(1, 1), 1);
+        assert.strictEqual(op.multiplica(), 1);
         assert.strictEqual(op.multiplica(-1, 1, -1, 1), 1);
         assert.strictEqual(op.multiplica(-1, 1, 0, -70), 0);
     });
